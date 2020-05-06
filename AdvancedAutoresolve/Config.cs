7e4b9af047af30134db43d0fc2f9a1b8e660f97b
ukie <irokez9696@gmail.com>
2020-05-06 12:19:27 -0400

    added config file

--- AdvancedAutoresolve/Config.cs
+++ AdvancedAutoresolve/Config.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,20 +11,39 @@ namespace AdvancedAutoResolve
 {
     internal class Config
     {
-        internal static Config CurrentConfig;
+        internal static Config CurrentConfig { get; private set; }
 
+        [JsonProperty]
         internal bool DetailedLogging { get; set; }
+        [JsonProperty]
         internal bool OnlyPlayerBattles { get; set; }
 
         internal bool ShouldLogThis(bool isPlayerInvolved = true) => DetailedLogging && (OnlyPlayerBattles && isPlayerInvolved || !OnlyPlayerBattles);
 
+        private static readonly string ConfigFilePath =
+            Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "config.json");
+
+        private static readonly bool configExists = File.Exists(ConfigFilePath);
+
         internal static void InitializeConfig()
         {
-            CurrentConfig = new Config()
+            var config = new Config()
             {
                 DetailedLogging = true,
                 OnlyPlayerBattles = true
             };
+
+            try
+            {
+                if (configExists)
+                {
+                    config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(ConfigFilePath));
+                }
+            }
+            finally
+            {
+                CurrentConfig = config;
+            }
         }
     }
 }
